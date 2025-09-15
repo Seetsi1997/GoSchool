2d61864be0385d444ee2e39abe8510d3305b760f
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2025-09-15 15:13:24 +0200

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/example/GoSchool/GoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoSchool/GoSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
